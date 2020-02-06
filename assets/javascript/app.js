94ebe7135b990b99c519bef7b6159d34685e2971
K Uyeda <kellikells@gmail.com>
2020-02-05 19:04:50 -0800

    stylized inout form, working on submit button function (grab info)

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+
+
+// ======== FUNCTION: ON,CLICK ADD TRAIN =======
+// ==============================================
+$(button).on("click", function() {
+    var tName = $("#train-name").val().trim();
+    var tDestination = $("#destination").val().trim();
+    var tFrequency = $("#frequency").val().trim();
+    
+    // to be calculated:
+    var arrivalTime = $("#arrival-time");
+    var minutesLeft = $("#minutes-left");
+
+})
