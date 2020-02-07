2e5609a66e97d8260207eebc6d7b332485401268
K Uyeda <kellikells@gmail.com>
2020-02-06 18:12:15 -0800

    firebase initialized. initial values declared. starting button function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,5 @@
 
 
-
-
 //========== INITIALIZE FIREBASE =========
 // --- web app's firebase configuration
 var config = {
@@ -17,7 +15,7 @@ var config = {
 firebase.initializeApp(config);
 
 // --- reference to firebase database (for convenience)
-var data = firebase.database();
+var database = firebase.database();
 
 // ============= INITIAL VALUES: ==============
 var name = "";
@@ -25,33 +23,46 @@ var destination = "";
 var frequency = 0;            //in minutes
 var firstTrain = 0;           //HH:mm
 
+console.log(name, destination, frequency, firstTrain);
+
 // ======== FUNCTION: ON,CLICK ADD TRAIN =======
 // ==============================================
-$(button).on("click", function(event) {
-    
+$("button").on("click", function (event) {
+
     event.preventDefault();
 
     name = $("#train-name-input").val().trim();
     destination = $("#destination-input").val().trim();
-    frequency = parseInt($("#frequency-input").val().trim());   
-    firstTrain = parseInt($("#first-train-input").val().trim());    
-    
+    frequency = parseInt($("#frequency-input").val().trim());
+    firstTrain = $("#first-train-input").val().trim();
 
-//check code works;
+    //check click function works;
     console.log(name);
     console.log(destination);
     console.log(frequency);
     console.log(firstTrain);
 
+    // ========= SET/PUSH data to database =========
 
-});
+    database.ref().push({
+        name: name,
+        destination: destination,
+        frequency: frequency,
+        firstTrain: firstTrain,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
 
-    // --- create <td>s for: 
+    // ==============================================
+
+
+})
+
+    // --- create <td>s for:
     // name, destination, frequency, next train time, mins away
 
 //     var nameData = $("<td>").text(tName);
 //     var destinationData = $("<td>")
-    
+
 
 
 //        // --- create <tr> to hold <td>s:
